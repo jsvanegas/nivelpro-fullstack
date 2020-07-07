11f13829e2418495e437c055ed20bdd52df039d9
jsvanegas <sebastian44van@gmail.com>
2020-07-07 09:08:43 -0500

    Agregar Ejemplo de conexion a MongoDB

--- intro-node/server-express.js
+++ intro-node/server-express.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 
+const MongoClient = require('mongodb').MongoClient;
+let libDb = null;
+
 const fileManager = require('./file-manager');
 
 const app = express();
@@ -9,11 +12,39 @@ app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: null }));
 app.set('view engine', 'hbs');
 
 
-
+// RENDER DE VISTAS
 app.get('/author-books/:author', (req, res) => {
     const books = fileManager.listBooks(req.params.author);
     res.status(200)
-        .render('books', { books: books })
+        .render('books', { title: 'Pagina de libros', books: books }) // render('path_archivo.hbs', {informacion dinamica que se le envia a la pagina})
+});
+// RENDER DE VISTAS
+
+// API :: JSON
+app.get('/authors', (req, res) => {
+
+    // Consultar todos los datos desde la BD
+    const authorsCol = libDb.collection('authors');
+    authorsCol.find({}, (err, cursor) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send({ error: err.message });
+            return;
+        }
+
+        // To Array para obtener los datos en forma de Arreglo en JS
+        cursor.toArray((parseError, result) => {
+            if (parseError) {
+                console.log(parseError);
+                res.status(500).send({ error: parseError.message });
+                return;
+            }   
+
+            res.status(200).send(result);
+
+        });
+    });
+
 });
 
 
@@ -34,8 +65,23 @@ app.get('/add-books', (req, res) => {
     res.status(200)
         .send(fileManager.addBook(author, title));
 });
-
+// API :: JSON
 
 app.listen(9090, () => {
     console.log('Server is running on port 9090');
+
+    const mongoDbPath = 'mongodb://localhost:27017';
+    const mongoConf = { useNewUrlParser: true, useUnifiedTopology: true };
+    MongoClient.connect(mongoDbPath, mongoConf, (err, client) => {
+
+        if (err) {
+            console.log('Error connecting to Database: ', err);
+            return;
+        }
+
+        console.log('Connection State: ', client.isConnected());
+        libDb = client.db('library');
+    });
+
+
 });
