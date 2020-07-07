adc3d0acd046151791a61e0cd606d61103350a14
Estefania Otalvaro Mora <estefania.otalvaro@cadena.com.co>
2020-07-07 09:32:44 -0500

    Integrar Node con MongoDB

--- intro-node-estefania/server-express.js
+++ intro-node-estefania/server-express.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 
+const MongoClient = require('mongodb').MongoClient;
+let libDb = null;
+
 const fileManager = require('./file-manager');
 
 const app = express();
@@ -8,10 +11,47 @@ const app = express();
 app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: null }));
 app.set('view engine', 'hbs');
 
+
+app.get('/authors', (req,res) => {
+    //Consultar los datos desde la BD
+
+    const authorsCol = libDb.collection('authors');
+
+    authorsCol.find({}, (err, cursor) => {
+        if(err){
+            console.log(err);
+            res.status(500).send({err:err.message });
+            return;
+        }
+
+        //ToArray para obterner los datos en forma de arreglo en JS
+
+        cursor.toArray((parseError, result) => {
+            if(err){
+                console.log(parseError);
+                res.status(500).send({error:parseError.message });
+                return;
+            }
+
+            res.status(200).send(result);
+    
+        });
+
+    });
+});
+
+
+
+
+
+
+
+
+
 app.get('/author-books/:author', (req,res) => {
     const books = fileManager.listBooks(req.params.author);
     res.status(200)
-        .render('books', {books: books});
+        .render('books', {title: 'Página de libros', books: books});
 });
 
 app.get('/books', (req,res) => {
@@ -43,5 +83,19 @@ app.get('/add-books', (req,res) => {
 
 app.listen(9090, () => {
     console.log('Server is running on port 9090');
+
+    const mongoDbPath = 'mongodb://localhost:27017';
+    const mongoConf = { useNewUrlParser: true, useUnifiedTopology: true};
+    MongoClient.connect(mongoDbPath, mongoConf, (err, client) => {
+
+        if(err){
+            console.log('Error connecting to Database: ', err);
+            return;
+        }
+
+        console.log('Connection State: ', client.isConnected());
+        libDb = client.db('library');
+
+    });
 });
 
