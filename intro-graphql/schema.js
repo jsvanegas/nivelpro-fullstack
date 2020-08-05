0d5018d95279a8e75f5d21b0106a3b5b86db9404
jsvanegas <sebastian44van@gmail.com>
2020-08-05 08:11:31 -0500

    Agregar Ejemplo de Mutations y Relaciones

--- intro-graphql/schema.js
+++ intro-graphql/schema.js
@@ -5,9 +5,10 @@ const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
-  GraphQLList
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull
 } = graphql;
-
 
 
 const UserType = new GraphQLObjectType({
@@ -17,11 +18,26 @@ const UserType = new GraphQLObjectType({
     name: { type: GraphQLString },
     email: { type: GraphQLString },
     country: { type: GraphQLString },
+    age: { type: GraphQLInt },
     company: {
       type: CompanyType,
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
           .then(res => res.data);
+      }
+    },
+    friends: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        const friendsIds = parentValue.friends;
+        if (!Array.isArray(friendsIds) || friendsIds.length === 0) {
+          return;
+        }
+
+        const paths = friendsIds.map(id => axios.get(`http://localhost:3000/users/${id}`));
+        return axios.all(paths).then(responses => {
+          return responses.map(res => res.data)
+        });
       }
     }
   })
@@ -63,7 +79,49 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const Mutations = new GraphQLObjectType({
+  name: 'Mutations',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: GraphQLInt }
+      },
+      resolve(parentValue, args) {
+        const { name, email, age } = args;
+        return axios.post(`http://localhost:3000/users`, { name, email, age })
+          .then(res => res.data);
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: { id: { type: new GraphQLNonNull(GraphQLString) } },
+      resolve(parentValue, args) {
+        return axios.delete(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
+      }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: GraphQLInt },
+        email: { type: GraphQLString }
+        // Tarea: Editar Amigos
+      },
+      resolve(parentValue, args) {
+        // usamos patch en lugar de put, porque se hace una actualización parcial de datos
+        // PUT: Actualiza TODA la entidad
+        return axios.patch(`http://localhost:3000/users/${args.id}`, { age: args.age, email: args.email })
+          .then(res => res.data);
+      }
+    }
+  }
+});
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutations
 });
